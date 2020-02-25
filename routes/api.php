3e9eab8895cc2418bcdda8279211646222a59f4c
vineet_dikonia <vineet19universal@gmail.com>
2020-02-25 13:24:58 +0530

    Removed extra route

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,7 @@ Route::post('/register', 'Api\AuthController@register');
 Route::post('/login', 'Api\AuthController@login');
 
 Route::group(['middleware' => 'auth:api'], function(){
-    Route::get('/profile', 'Api\AuthController@profile');
-    
+    Route::resource('/cruds', 'CrudsController', [
+        'except' => ['edit', 'show', 'store']
+    ]); 
 });
-Route::resource('/cruds', 'CrudsController', [
-    'except' => ['edit', 'show', 'store']
-]);
